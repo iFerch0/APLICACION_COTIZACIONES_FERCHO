d34ecfaa30c2ca01fb5736bed8643c63ad2af42d
Fernando <rhenalsf@gmail.com>
2026-04-07 09:03:06 -0500

    Prepare deploy

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: '/cotizaciones',
   reactStrictMode: true,
 
   serverExternalPackages: ["@prisma/client", "prisma"],
